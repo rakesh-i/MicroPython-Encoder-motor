641e0c3800ae401bdd9ad2cca0a09bba265a91db
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2021-10-06 11:12:15 +0530

    Update set_target.py

--- set_target.py
+++ set_target.py
@@ -5,20 +5,21 @@ import motor
 p1 = Pin(27, Pin.IN)
 p2 = Pin(26, Pin.IN)
 
-target = 10000
-speed = 500
+target = 10000          #Target position of encoder
+speed = 500         
 last_pos = 0
 w = 0
 KP = .12
 KD = .1
 KI = .00005
-max_correction = 500
+max_correction = 500    #Maximum speed of the motor 
 
 pos = 0
 
 def convert(x, i_m, i_M, o_m, o_M):
     return max(min(o_M, (x - i_m) * (o_M - o_m) // (i_M - i_m) + o_m), o_m)
 
+#
 def correction(revolutions):
     if revolutions > 0:
         if revolutions > max_correction:
@@ -32,6 +33,7 @@ def correction(revolutions):
             revolutions = revolutions 
     return revolutions
 
+#Swap encoder pins if pos(position counter) value doesn't reduce when we reverse the direction of motor.
 def handle_interrupt(pin):
     global pos
     a = p2.value()
@@ -40,7 +42,8 @@ def handle_interrupt(pin):
     else:
         pos = pos-1
 
-p1.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+#interrpt handler(triggers interrupt when encoder 1 on Pin 27 goes high)
+p1.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt) 
 
 while(1):
     propotional = target- pos
@@ -52,5 +55,5 @@ while(1):
     last_pos = pos
     print(pos, r)
     
-    time.sleep(.1)
+    time.sleep(.1) #Add atleast .01sec delay for stable terminal
 
