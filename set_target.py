b020939df7989a90d09a15957a6265b0ead803ae
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-08-17 10:33:29 +0530

    Update set_target.py

--- set_target.py
+++ set_target.py
@@ -2,24 +2,15 @@ from machine import Pin
 import time
 import motor
 
-p1 = Pin(27, Pin.IN) //enabel pin C1 (interchange the pins if the values are in reverse)
-p2 = Pin(26, Pin.IN) //enable pin C2
+p2 = Pin(14, Pin.IN)
+p1 = Pin(27, Pin.IN)
 
-target = 10000          #Target position of encoder
-speed = 500         
-last_pos = 0
-w = 0
-KP = .12
-KD = .1
-KI = .00005
-max_correction = 500    #Maximum speed of the motor 
-
+max_correction = 1000    #Maximum correction speed of the motor 
 pos = 0
 
 def convert(x, i_m, i_M, o_m, o_M):
     return max(min(o_M, (x - i_m) * (o_M - o_m) // (i_M - i_m) + o_m), o_m)
 
-#
 def correction(revolutions):
     if revolutions > 0:
         if revolutions > max_correction:
@@ -45,15 +36,21 @@ def handle_interrupt(pin):
 #interrpt handler(triggers interrupt when encoder 1 on Pin 27 goes high)
 p1.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt) 
 
-while(1):
-    propotional = target- pos
-    derivative = target - last_pos
-    integral = target +last_pos
-    rotate = (propotional*KP + derivative*KD + integral*KI)
-    r = int(correction(rotate))
-    motor.motorSpeed(r)
-    last_pos = pos
-    print(pos, r)
-    
-    time.sleep(.1) #Add atleast .01sec delay for stable terminal
-
+def target(set):
+	last_pos = 0
+	speed = 500         
+	w = 0
+	KP = 1
+	KD = .1
+	KI = .00005
+	while(1):
+	    propotional = set- pos
+	    derivative = set - last_pos
+	    integral = set +last_pos
+	    rotate = (propotional*KP + derivative*KD + integral*KI)
+	    r = int(correction(rotate)) #Motor output speed(0-1000)
+	    motor.motorSpeed(r) 
+	    last_pos = pos              #current positon of the encoder.
+	    print(pos, r)
+	    
+	    time.sleep(.1) #Add atleast .01sec delay for stable terminal
