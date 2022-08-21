b6d59709b5cb7518259565bb4be2c8dd9eb50963
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-08-21 18:56:16 +0530

    Create multi_motor.py

---
+++ multi_motor.py
@@ -0,0 +1,13 @@
+from encoder_N20_esp.py import PID, Motor
+
+# Creating objects of each motor
+m1 = Motor(21, 22, 23, 16, 4)
+m2 = Motor(19, 18, 5, 14, 27)
+
+# Creating PID objects for each motor
+p1 = PID(3,.1,0.001,800) 
+p2 = PID(3,.1,0.001,800)
+
+while(1):
+  p1.setTarget(1000, m1)
+  p2.setTarget(-1000, m2)
