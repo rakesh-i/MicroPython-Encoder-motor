eb7deb76c75fefd2bbb38dca73f16e431b7c4125
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-08-22 03:55:29 +0530

    Update multi_motor.py

--- multi_motor.py
+++ multi_motor.py
@@ -5,9 +5,9 @@ m1 = Motor(21, 22, 23, 16, 4)
 m2 = Motor(19, 18, 5, 14, 27)
 
 # Creating PID objects for each motor
-p1 = PID(5,.1,0.001,800) 
-p2 = PID(5,.1,0.001,800)
+p1 = PID(m1, 5, 0.1, 0.001, 800) 
+p2 = PID(m2, 5, 0.1, 0.001, 800)
 
 while(1):
-  p1.setTarget(1000, m1)
-  p2.setTarget(-1000, m2)
+  p1.setTarget(1000)
+  p2.setTarget(-1000)
